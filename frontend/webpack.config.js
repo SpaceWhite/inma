f3c5fee8ca7469c9352f26ef0e5d09a4a95c51fd
SpaceWhite <space_white@yahoo.com>
2023-09-03 14:25:15 +0900

    fix frontend

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,24 +1,36 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        map: './src/page/map.js',
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-		filename: 'index.js'
+		filename: 'main.js'
 	},
     plugins: [
         new CopyWebpackPlugin({
             patterns: [
-                { from: 'css' }
+                { from: 'css', to: 'css' }
             ]
         }),
         new CopyWebpackPlugin({
             patterns: [
-                { from: 'js' }
+                { from: 'js', to: 'js' }
             ]
-        })
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'asset', to: 'asset' }
+            ]
+        }),
+        new HtmlWebpackPlugin({
+	    filename: "map.html",
+            template: 'html/map.html'
+	}),
     ],
     resolve: {
       fallback: { "querystring": require.resolve("querystring-es3") }
