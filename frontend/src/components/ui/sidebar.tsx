a7a43059bd601b8977bcd341bd95883872422b8b
SpaceWhite <space_white@yahoo.com>
2023-09-10 09:39:15 +0900

    fix docker

--- frontend/src/components/ui/sidebar.tsx
+++ frontend/src/components/ui/sidebar.tsx
@@ -9,13 +9,13 @@ function Sidebar({hospitals}: SidebarProps) {
 		<aside className="fixed top-0 left-0 z-40 w-2/5 h-screen pt-100 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0" aria-label="Sidebar">
 			<div className="h-full pt-3 px-3 pb-4 overflow-y-auto bg-white">
 				<div className="grid grid-cols-2 gap-4">
-          {hospitals.map(hospital => 
-					<div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
+          {hospitals.map((hospital, index) => 
+					<div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow" key={index}>
 						<h5 className="mb-2 text-l font-bold tracking-tight text-gray-900">{hospital.name}</h5>
 						    <p className="mb-3 font-normal text-gray-700">{hospital.address}</p>
 						    <p className="text-xm mb-3 font-normal text-gray-700">Language :
-                  {hospital.languages.map(language =>  
-                  <span className="text-violet-700 font-bold">
+                  {hospital.languages.map((language, langIndex) =>  
+                  <span className="text-violet-700 font-bold" key={langIndex}>
                   {language}, </span>)} jp
                 </p>
 					</div>
